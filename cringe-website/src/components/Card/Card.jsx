b5127947bbe24edf125911cd11fa959769144eb7
jyeh20 <jmyeh51@gmail.com>
2022-03-22 17:54:52 -0700

    more cringe

--- cringe-website/src/components/Card/Card.jsx
+++ cringe-website/src/components/Card/Card.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 
 import { getRandomInt } from "../../assets/utils"
 
@@ -6,6 +6,21 @@ import "./Card.css"
 
 const Card = (props) => {
   const { title, content, index, cringeMode } = props
+  const [clicked, setClicked] = useState(false)
+  console.log(clicked)
+
+  const animateCardOnce = () => {
+    const choice = getRandomInt(0, 2)
+    const duration = (1, 3) / 2
+    const direction = getDirection()
+
+    switch (choice) {
+      case 0:
+        return `spinX 1 ${duration}s ${direction}`
+      case 1:
+        return `spin 1 ${duration}s ${direction}`
+    }
+  }
 
   const getDirection = () => {
     const choice = getRandomInt(0, 2)
@@ -16,7 +31,7 @@ const Card = (props) => {
     }
   }
 
-  const getCardAnimation = () => {
+  const animateCard = () => {
     const choice = getRandomInt(0, 2)
     const duration = getRandomInt(5, 6)
     const delay = getRandomInt(2, 6)
@@ -36,6 +51,15 @@ const Card = (props) => {
     return `scale-text infinite ${duration}s ${delay}s ${direction}`
   }
 
+  const getCardAnimation = () => {
+    if (cringeMode) {
+      return animateCard()
+    } else if (clicked) {
+      return animateCardOnce()
+    }
+    return "none"
+  }
+
   return (
     <div
       className="Card"
@@ -43,10 +67,22 @@ const Card = (props) => {
         color: cringeMode ? "#FF0000" : "",
         gridColumnStart: 2 * index + 2,
         borderRadius: cringeMode ? "0px" : "20px",
-        animation: cringeMode ? getCardAnimation() : "none",
+        animation: getCardAnimation(),
         border: cringeMode ? "3px solid #ff0000" : "1px solid #d7d6d6",
         backgroundColor: cringeMode ? "#ffff00" : "rgba(255, 255, 255, 0.1)",
         backdropFilter: cringeMode ? "none" : "blur(3px)",
+        cursor: cringeMode ? "default" : "pointer",
+        fontFamily: cringeMode ? "Times New Roman" : "",
+      }}
+      onClick={() => {
+        if (!cringeMode) {
+          setClicked(true)
+        }
+      }}
+      onAnimationEnd={() => {
+        if (!cringeMode) {
+          setClicked(false)
+        }
       }}
     >
       <div
